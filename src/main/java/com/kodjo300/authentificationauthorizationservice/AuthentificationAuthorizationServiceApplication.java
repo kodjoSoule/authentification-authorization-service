f3361dd8fcb199492328e9f13aa85128bfe475d1
Souleymane KODJO <76671691+kodjoSoule@users.noreply.github.com>
2024-06-20 11:30:04 +0000

    First commit.

---
+++ src/main/java/com/kodjo300/authentificationauthorizationservice/AuthentificationAuthorizationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.kodjo300.authentificationauthorizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationAuthorizationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthentificationAuthorizationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodjo300/authentificationauthorizationservice/AuthentificationAuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodjo300.authentificationauthorizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentificationAuthorizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
